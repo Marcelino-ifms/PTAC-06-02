130762942d5e3386fe833d2bd150b534c49a8db6
MatheusKiyota <161073663+MatheusKiyota@users.noreply.github.com>
2024-03-09 19:34:25 -0300

    ImportHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
+import Footer from "./components/Footer";
+
 export default function App() {
-
-  const nome = "João";
-  const sobreNome = "Santos";
-
   return(
-    <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
-    </div>  
+   
+    <main>
+        <Footer/>
+    </main>
+  
   );
-
-
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer>
+            <h1>Matheus Leite Kiyota</h1>
+        </footer>
+    );
+}
