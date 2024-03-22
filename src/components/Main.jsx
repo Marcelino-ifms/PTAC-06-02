5148784382389ff3a0deada2efed2a4f84a45c2b
MatheusKiyota <matheus.kiyota@estudante.ifms.edu.br>
2024-03-22 18:06:04 -0300

    AtividadeEmail

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,32 +3,67 @@ import { useState } from "react";
 export default function Main(){
   const[nome, setNome] = useState("") //(0) number  // (true) boolean
   const[telefone, setTelefone] = useState(0)
+  const[email, setEmail] = useState("")
+  const[listaContatos, setListaContatos] = useState([]) //lista usamos cochetes[]
+
+
+  const registrar = (event) => {
+    event.preventDefault();
+    setListaContatos([...listaContatos,
+      
+      {
+        nomeSalvo: nome,
+        telefoneSalvo: telefone,
+        emailSalvo: email,
+      }
+
+    ]);
+  };
+
+  console.table(listaContatos)
     return(
       <main>
 
-<label> Nome:
-         <input
-         type="text"
-         name="" id=""
-         onChange={(event)=> setNome(event.target.value)}
-         />
-         </label>
-         <button>Cadastre-se</button>
-         {nome}
+                <form action=""  onSubmit={registrar}>
+                <label htmlFor="nome"> Nome:  </label>
+                <input
+                type="text"
+                name="" id=""
+                value={nome}
+                onChange={(event)=> setNome(event.target.value)}
+                />
+
+                <button>Cadastre-se</button>
+                {nome}
 
          <br/>
          <br/>
     
-<label> Telefone:
+         <label htmlFor="telefone"> Telefone:  </label>
          <input
-         type="text"
+         type="tel"
          name="" id=""
+         value={telefone}
          onChange={(event)=> setTelefone(event.target.value)}
          />
-         </label>
           <button>Cadastre-se</button>
          {telefone}
+         
+           <br/>
+           <br/>
 
+                <label htmlFor="email"> Email:  </label>
+                <input
+                type="email"
+                name="" id=""
+                value={email}
+                onChange={(event)=> setEmail(event.target.value)}
+                />
+                
+                  <button>Cadastre-se</button>
+                {email}
+
+         </form>
   
       </main>
     );
